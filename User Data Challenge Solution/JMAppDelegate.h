aa0e79714ed4542aa913d3ac7741850463667d5e
jonmariano21 <jonmariano21@gmail.com>
2014-06-20 00:03:19 -0700

    Initial Commit

---
+++ User Data Challenge Solution/JMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMAppDelegate.h
+//  User Data Challenge Solution
+//
+//  Created by JONATHAN MARIANO on 6/20/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
